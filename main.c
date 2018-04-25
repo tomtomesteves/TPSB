ef2c7a4396423fe1c027a88833c7d50de8524579
THOMAS ESTEVES <thomasesteves95@hotmail.com>
2018-04-25 11:01:58 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+  FILE* in,out;
+  in = fopen(,"r");
+  out = fopen(,"w");
+  fprintf(out, "%s\n", );
+
+  return 0;
+}
