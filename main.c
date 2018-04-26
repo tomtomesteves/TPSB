f6e0368fa6a36ed8cf82db89fa64a10e5083e4ff
tomtomesteves <thomasesteves95@hotmail.com>
2018-04-26 14:10:43 -0300

    Inicial

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-testee 
+
 int main(int argc, char const *argv[]) {
   FILE* in,out;
   in = fopen(,"r");
   out = fopen(,"w");
   fprintf(out, "%s\n", );
-  aigusahgsahugasuighsaughaiughiusahgiusahuighasghuiashg
-  teseeeee
   return 0;
 }
--- opcode.h
+++ opcode.h
@@ -4,6 +4,7 @@
 //Decodifica a instrucao para hexa
 int opcodeDecode(char* instruction);
 
+//tranforma int em binario
 char *IntToBin(int a);
 
 #endif
