18bf3c6d1efa47a935f4329d7af58d52d0c62914
tomtomesteves <thomasesteves95@hotmail.com>
2018-04-26 14:41:32 -0300

    opcode pronto

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "opcode.h"
 
 int main(int argc, char const *argv[]) {
-  FILE* in,out;
-  in = fopen(,"r");
-  out = fopen(,"w");
-  fprintf(out, "%s\n", );
+  // FILE* in,out;
+  // in = fopen(,"r");
+  // out = fopen(,"w");
+  // fprintf(out, "%s\n", );
+  printf("%s\n",IntToBinOP(opcodeDecode("jump")) ); 
   return 0;
 }
